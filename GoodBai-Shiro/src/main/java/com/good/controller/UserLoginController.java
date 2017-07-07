42c9516bd4e98292f4db5b6a193296b2eff61eb3
hellojol <hellojol@163.com>
2017-07-07 15:47:23 +0800

    [xxxx]

---
+++ GoodBai-Shiro/src/main/java/com/good/controller/UserLoginController.java
@@ -0,0 +1,25 @@
+package com.good.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.good.base.JSONUtil;
+
+@Controller
+public class UserLoginController {
+
+	@RequestMapping("/login/loginsubmit.do")
+	public @ResponseBody String queryAll() throws Exception {
+		// TODO Auto-generated method stub
+		Map<String,Object> map =new HashMap<String,Object>();
+		map.put("total", 1);
+		map.put("rows", null);
+		map.put("page", 2);
+		System.out.println("----- UserLoginController-queryAll");
+		return JSONUtil.toJsonString(map);
+	}
+}
