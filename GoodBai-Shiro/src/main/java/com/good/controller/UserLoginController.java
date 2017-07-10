cb7c93b66ac9561d326a99cefab828f7c959906b
hellojol <hellojol@163.com>
2017-07-10 17:28:56 +0800

    1111

--- GoodBai-Shiro/src/main/java/com/good/controller/UserLoginController.java
+++ GoodBai-Shiro/src/main/java/com/good/controller/UserLoginController.java
@@ -1,24 +1,21 @@
 package com.good.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserLoginController {
 
 	@RequestMapping("/login/loginsubmit.do")
-	public @ResponseBody String queryAll() throws Exception {
-		// TODO Auto-generated method stub
-		Map<String,Object> map =new HashMap<String,Object>();
-		map.put("total", 1);
-		map.put("rows", null);
-		map.put("page", 2);
+	public ModelAndView  queryAll(ModelMap modelMap) throws Exception {
+		// TODO Auto-generated method stub	
+		modelMap.put("total", 1);
+		modelMap.put("rows", null);
+		modelMap.put("page", 2);
 		System.out.println("----- UserLoginController-queryAll");
 		//JSONUtil.toJsonString(map)
-		return "view/index2";
+		return new ModelAndView("index2");
 	}
 }
