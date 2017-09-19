ddf926669dcf9be9280a731553b8380759be3838
mag <mag@kongtrolink.com>
2017-09-19 16:04:21 +0800

    111

--- GoodBai-Shiro-View/src/App.vue
+++ GoodBai-Shiro-View/src/App.vue
@@ -49,15 +49,15 @@
                             导航一
                         </template>
                         <MenuItem name="1-1"><router-link to="/home">选项 1x1</router-link></MenuItem>
-                        <MenuItem name="1-2">选项 2</MenuItem>
-                        <MenuItem name="1-3">选项 3</MenuItem>
+                        <MenuItem name="1-2" href="/home">选项 2x</MenuItem>
+                        <MenuItem name="1-3" href="/menu3">选项 3</MenuItem>
                     </Submenu>
                     <Submenu name="2">
                         <template slot="title">
                             <Icon type="ios-keypad"></Icon>
                             导航二
                         </template>
-                        <MenuItem name="2-1">选项 1</MenuItem>
+                        <MenuItem name="2-1"><router-link to="/menu3">选项 2x2</router-link></MenuItem>
                         <MenuItem name="2-2">选项 2</MenuItem>
                     </Submenu>
                     <Submenu name="3">
