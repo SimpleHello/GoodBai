51be23367673999c42fcd65fb304a95e755189f2
mag <mag@kongtrolink.com>
2017-09-27 17:42:43 +0800

    11111

--- GoodBai-Shiro-View/src/App.vue
+++ GoodBai-Shiro-View/src/App.vue
@@ -1,112 +1,17 @@
-<style scoped>
-    .layout{
-        border: 1px solid #d7dde4;
-        background: #f5f7f9;
-        position: relative;
-    }
-    .layout-breadcrumb{
-        padding: 10px 15px 0;
-    }
-    .layout-content{
-        height: -webkit-fill-available;
-        overflow: hidden;
-        background: #fff;
-        border-radius: 4px;
-    }
-    .layout-content-main{
-        padding: 10px;
-    }
-    .layout-copy{
-        text-align: center;
-        padding: 10px 0 20px;
-        color: #9ea7b4;
-    }
-    .layout-menu-left{
-        background: #464c5b;
-    }
-    .layout-header{
-        height: 20px;
-        background: #fff;
-        box-shadow: 0 1px 1px rgba(0,0,0,.1);
-    }
-    .layout-logo-left{
-        width: 90%;
-        height: 30px;
-        background: #5b6270;
-        border-radius: 3px;
-        margin: 15px auto;
-    }
-</style>
 <template>
-    <div class="layout">
-        <Row type="flex">
-            <Col span="5" class="layout-menu-left">
-                <Menu active-name="1-1" theme="dark" width="auto" :open-names="['1']" @on-select="onMenuSelected">
-                    <div class="layout-logo-left"></div>
-                    <Submenu name="1">
-                        <template slot="title">
-                            <Icon type="ios-navigate"></Icon>
-                            导航一
-                        </template>
-                        <MenuItem name="1-1">选项 11111</MenuItem>
-                        <MenuItem name="1-2">选项 2x1</MenuItem>
-                        <MenuItem name="1-3">选项 3</MenuItem>
-                    </Submenu>
-                    <Submenu name="2">
-                        <template slot="title">
-                            <Icon type="ios-keypad"></Icon>
-                            导航二
-                        </template>
-                        <MenuItem name="2-1"><router-link to="/menu3">选项 2x2</router-link></MenuItem>
-                        <MenuItem name="2-2" @click="show">选项 2show</MenuItem>
-                    </Submenu>
-                    <Submenu name="3">
-                        <template slot="title">
-                            <Icon type="ios-analytics"></Icon>
-                            导航三
-                        </template>
-                        <MenuItem name="3-1">选项 1</MenuItem>
-                        <MenuItem name="3-2">选项 2</MenuItem>
-                    </Submenu>
-                </Menu>
-            </Col>
-            <Col span="19">
-                <div class="layout-header"></div>
-                <div class="layout-breadcrumb">
-                    <Breadcrumb>
-                        <BreadcrumbItem href="#">首页</BreadcrumbItem>
-                        <BreadcrumbItem href="#">应用中心</BreadcrumbItem>
-                        <BreadcrumbItem>某应用</BreadcrumbItem>
-                    </Breadcrumb>
-                </div>
-                <div class="layout-content">
-                     <router-view></router-view>
-                </div>
-                <!--<div class="layout-copy">
-                    2011-2016 &copy; TalkingData
-                </div> -->
-            </Col>
-        </Row>
-    </div>  
+    <div>
+        <router-view></router-view>
+        <Back-top :bottom="100"></Back-top>
+    </div>
 </template>
 <script>
     export default {
-        methods:{
-            onMenuSelected:function(path){
-                console.log(path);
-                if("1-1"==path){
-                     console.log("1-1 jinru l ");
-                    this.$router.push({path:"/menu11"});
-                }else if("1-2"==path){
-                    this.$router.push({path:"/menu12"});
-                }else{
-                    alert(path+" > 这个是无效链接");
-                }
-                // 
-            },
-            show:function(){
-                alert("show");
+        data () {
+            return {
+
             }
+        },
+        methods: {
         }
     }
 </script>
--- GoodBai-Shiro-View/src/router/index.js
+++ GoodBai-Shiro-View/src/router/index.js
@@ -1,15 +1,24 @@
 import Vue from 'vue'
-import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import VueRouter from 'vue-router'
+import Menu11 from "../components/Menu11"
+import Menu12 from '../components/Menu12'
+import Home from '../components/Home'
 
-Vue.use(Router)
+Vue.use(VueRouter)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
-  ]
-})
+const routes = [{
+  path : '/home',
+  component : Home
+},{
+  path : '/menu11',
+  component : Menu11
+},{
+  path : '/menu12',
+  component : Menu12
+}];
+
+const router = new VueRouter({
+  routes
+});
+
+export default router;
