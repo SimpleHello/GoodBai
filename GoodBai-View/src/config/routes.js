44b35dd6285d0006ed7703f98b8b3832702f8bdd
mag <mag@kongtrolink.com>
2017-09-30 17:16:12 +0800

    111111

--- GoodBai-View/src/config/routes.js
+++ GoodBai-View/src/config/routes.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import {ymzs,getUrl} from '../module/leftNav.js';
 
 Vue.use(VueRouter)
 
@@ -11,13 +12,26 @@ const routes = [{
   name:'home',
   component:resolve=>require(['@/page/home.vue'],resolve),
   children:[
-    {path:'/home/user', component:resolve=>require(['@/page/system/user.vue'],resolve)},
+    {path:'/sys/userinfo', component:resolve=>require(['@/page/system/user.vue'],resolve),meta: {requiresAuth: true}},
     {path:'*',component:resolve=>require(['@/page/404.vue'],resolve)}
   ]
-}];
+}
+];
 
 const router = new VueRouter({
   routes
 });
+
+router.beforeEach((to, from, next) => {
+  // 可以在路由元信息指定哪些页面需要登录权限
+  let sx = getUrl(ymzs.nav);
+  let path = to.path;
+  const islogin = sx.includes(path); // 假设没有登录（这里应从接口获取）
+  if (to.meta.requiresAuth && !islogin) { // 需要登录授权，这里还需要判断是否登录
+    next('/') // 跳转到登录
+    return
+  }
+  next()
+})
 
 export default router;
