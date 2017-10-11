e86af0afacdbb4a8ae24dc5adad0a51caf6dab00
mag <mag@kongtrolink.com>
2017-10-11 17:49:05 +0800

    111111

--- GoodBai-View/src/page/test/Tree1.vue
+++ GoodBai-View/src/page/test/Tree1.vue
@@ -1,7 +1,17 @@
 <template>
    <div>
-    <Button type="success" @click="saveRoleFun">新增</Button>
-    <Button type="success" @click="cancelRoleFun">还原</Button><hr style="margin-top:10px;margin-bottom: 10px">
+     <div>
+       <Row>
+         <Col span="12">
+           <p style="font-size: 14px;font-weight: 900">角色: {{roleName}}</p>
+         </Col>
+         <Col span="12">
+           <Button type="success" @click="saveRoleFun">新增</Button>
+           <Button type="success" @click="cancelRoleFun">返回</Button>
+         </Col>
+       </Row>
+       <hr style="margin-top:10px;margin-bottom: 10px">
+     </div>
     <ZTree
         ref='tree'
         :treeData="treeData"
@@ -18,74 +28,66 @@
     import axios from 'axios';
     import {ZTree} from '../../module/tree'
     Vue.use(ZTree);
-
+    const treeValue =  [
+      {
+        id: 1,
+        name: '一级节点',
+        open: true,
+        checked: false,
+        nodeSelectNotAll: false,//新增参数，表示父框可以半钩状态
+        parentId: null,
+        visible: true,
+        searched: false,
+        children: [
+          {
+            id: 1001,
+            name: '2级节点',
+            open: true,
+            checked: false,
+            nodeSelectNotAll: false,//新增参数，表示父框可以半钩状态
+            parentId: 1,
+            visible: true,
+            searched: false
+          },
+          {
+            id: 1002,
+            name: '2级节点',
+            open: true,
+            checked: false,
+            nodeSelectNotAll: false,//新增参数，表示父框可以半钩状态
+            parentId: 1,
+            visible: true,
+            searched: false
+          }
+        ]
+      },
+      {
+        id: 2,
+        name: '一级节点',
+        open: true,
+        checked: false,
+        nodeSelectNotAll: false,
+        parentId: null,
+        visible: true,
+        searched: false
+      },
+      {
+        id: 3,
+        name: '一级节点',
+        open: true,
+        checked: false,
+        nodeSelectNotAll: false,
+        parentId: null,
+        visible: true,
+        searched: false
+      }
+    ];
     const Tree1 = {
         name: 'Tree1',
         data: function () {
             return {
-                treeData: [
-                    {
-                        id: 1,
-                        name: '一级节点',
-                        open: true,
-                        checked: false,
-                        nodeSelectNotAll: false,//新增参数，表示父框可以半钩状态
-                        parentId: null,
-                        visible: true,
-                        searched: false,
-                        children: [
-                            {
-                                id: 1001,
-                                name: '2级节点',
-                                open: true,
-                                nodeSelectNotAll: false,//新增参数，表示父框可以半钩状态
-                                parentId: 1,
-                                visible: true,
-                                searched: false
-                            },
-                            {
-                                id: 1002,
-                                name: '2级节点',
-                                open: true,
-                                nodeSelectNotAll: false,//新增参数，表示父框可以半钩状态
-                                parentId: 1,
-                                visible: true,
-                                searched: false
-                            }
-                        ]
-                    },
-                    {
-                        id: 2,
-                        name: '一级节点',
-                        open: true,
-                        checked: false,
-                        nodeSelectNotAll: false,
-                        parentId: null,
-                        visible: true,
-                        searched: false
-                    },
-                    {
-                        id: 3,
-                        name: '一级节点',
-                        open: true,
-                        checked: false,
-                        nodeSelectNotAll: false,
-                        parentId: null,
-                        visible: true,
-                        searched: false
-                    }
-                ],
-              computed:{
-                treeData:function(){
-                  if(this.checkIds==1){
-
-                  }else if(this.checkIds==2){
-
-                  }else{
-                    return this.treeData;
-                  }
-                }
-               },
+                treeData: treeValue,
+                roleName:'',
                 options: {
                     labelKey: 'name',
                     showCheckbox: true,
@@ -98,18 +100,41 @@
                 },
             }
         },
-        methods: {
+      methods: {
             itemClick (node) {
                 console.log(node.id);
             },
             saveRoleFun(){
-              alert("新增按钮");
+              var _this=this;
+              let nodes = _this.$refs.tree.getSelectedNodeIds();
+              console.log(nodes);//选中的值
             },
            cancelRoleFun(){
              alert("还原");
            },
-        },
-      props:['checkIds']
+           roleInfoData(checkIds,roleName){
+             let data = treeValue;
+             var recurFunc = (datax,arr) => {
+               datax.forEach((i)=>{
+                 if(arr.indexOf(i.id)>=0){
+                 i.checked = true;
+               }else {
+                 i.checked = false;
+               }
+               if(i.children && i.children.length>0){
+                 recurFunc(i.children,arr);
+               }
+             });
+             }
+             if(checkIds==1){
+               recurFunc(data,[1001,1002]);
+             }else if(checkIds==2){
+               recurFunc(data,[3]);
+             }
+             this.roleName = roleName;
+             this.treeData = data;
+           }
+        }
     }
 
     export default Tree1;
