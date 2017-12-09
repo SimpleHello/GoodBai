8e0a3e24201f19f61491eeea671093674d6862e4
mag <mag@kongtrolink.com>
2017-12-09 17:13:43 +0800

    ztree的完美

--- GoodBai-rubbish/src/main/java/com.good.rubbish/App.java
+++ GoodBai-rubbish/src/main/java/com.good.rubbish/App.java
@@ -1,14 +1,19 @@
 package com.good.rubbish;
 
+import java.util.Arrays;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        String str = "330102000101700030217002001";
-        String code = "0"+str.substring(19,24);
-        String lastthee = code.substring(3);
-        System.out.println(code);
-        System.out.println(lastthee);
+        byte[] read = {0, 21, 0, 22, 3, 32, 3, 0, 1, -73, 53};
+        System.out.println( Arrays.toString(read));
+//        String str = "330102000101700030217002001";
+//        String code = "0"+str.substring(19,24);
+//        String lastthee = code.substring(3);
+//        System.out.println(code);
+//        System.out.println(lastthee);
     }
+
 }
