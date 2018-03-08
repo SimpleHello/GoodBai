ec38cd91f422ed516a26ce4772b6ee27a9f0ad6c
mag <mag@kongtrolink.com>
2018-03-08 16:27:03 +0800

    1111111111

--- UI/GoodBai-App/src/main.js
+++ UI/GoodBai-App/src/main.js
@@ -1,16 +1,28 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import Vonic from 'vonic/src/index.js'
+import FastClick from 'fastclick'
+import VueRouter from 'vue-router'
+import App from './App'
+import Home from './components/HelloFromVux'
 
-// Page Components
-import Index from './components/Index.vue'
-import About from './components/About.vue'
+Vue.use(VueRouter)
 
-// Routes
-const routes = [
-  { path: '/', component: Index },
-  { path: '/about', component: About }
-]
+const routes = [{
+  path: '/',
+  component: Home
+}]
 
-Vue.use(Vonic.app, {
-  routes: routes
+const router = new VueRouter({
+  routes
 })
+
+FastClick.attach(document.body)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app-box')
