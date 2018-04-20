e835a6bb3e65901b569abc352a23ef530d258463
mag <mag@kongtrolink.com>
2018-04-20 17:12:23 +0800

    1111111111

--- GoodBai-rubbish/src/main/java/com.good.rubbish/App1.java
+++ GoodBai-rubbish/src/main/java/com.good.rubbish/App1.java
@@ -12,9 +12,9 @@ public class App1 {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         Calendar calendar = Calendar.getInstance();
         Date date = new Date();
-        System.out.println("当前时间:"+sdf.format(date));
-        calendar.setTime(date);
-        calendar.add(Calendar.MINUTE , -5 ) ; //5分钟之前;
+//        System.out.println("当前时间:"+sdf.format(date));
+//        calendar.setTime(date);
+//        calendar.add(Calendar.MINUTE , -5 ) ; //5分钟之前;
         System.out.println("5分钟前:"+sdf.format(calendar.getTime()));
 //        System.out.println(calendar.get(Calendar.YEAR));
 //        System.out.println(calendar.get(Calendar.MONTH)+1);
@@ -22,5 +22,7 @@ public class App1 {
 //        System.out.println(calendar.get(Calendar.HOUR_OF_DAY));
 //        System.out.println(calendar.get(Calendar.MINUTE));
 //        System.out.println(calendar.get(Calendar.SECOND));
+        //将时间点设置为次月1号0点
+
     }
 }
